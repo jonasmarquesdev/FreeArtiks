e6ddf0b78870f40eb6c2fdfc6f0b143007b97c90
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-10-19 06:55:21 -0300

    Feat: Refatorando contexto de produto

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, { createContext, useContext, useState } from "react";
-import { image1, image2, image3, image4, image5, image6 } from "./assets";
+import { image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12 } from "./assets";
 
 const LivroContext = createContext();
 const ArtigoContext = createContext();
@@ -11,42 +11,72 @@ const ArtigoContext = createContext();
 export function LivroProvider({ children }) {
   const livrosCadastrados = [
     {
-      id: 1,
       titulo: "Sistema de Banco de Dados",
       image: image1,
       autor: "Abraham Silberschatz, Henry F. Korth e S. Sudarshan",
       categoria: "Introdutório",
     },
     {
-      id: 2,
       titulo: "Sistemas de Banco de Dados",
       image: image2,
       autor: "Ramez Elmasri",
       categoria: "Introdutório",
     },
     {
-      id: 3,
+      titulo: "Introdução à Inteligência Artificial",
+      image: image8,
+      autor: "Tom Taulli",
+      categoria: "Introdutório",
+    },
+    {
+      titulo: "Smashing HTML5",
+      image: image12,
+      autor: "Bill Sanders",
+      categoria: "Introdutório",
+    },
+    {
       titulo: "Projeto de Banco de Dados: Volume 4",
       image: image3,
       autor: "Carlos Alberto Heuser",
       categoria: "Importante",
     },
     {
-      id: 4,
       titulo: "Banco De Dados - Projetos E Implementação",
       image: image4,
       autor: "FELIPE NERY RODRIGUES MACHADO",
       categoria: "Importante",
     },
     {
-      id: 5,
+      titulo: "Comunicação de Dados e Redes de Computadores",
+      image: image7,
+      autor: "Behrouz A. Forouzan",
+      categoria: "Importante",
+    },
+    {
+      titulo: "Redes de Computadores",
+      image: image9,
+      autor: "Tanenbaum e Wetherall",
+      categoria: "Importante",
+    },
+    {
+      titulo: "Algoritmos teoria e prática",
+      image: image10,
+      autor: "Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, Clifford Stein",
+      categoria: "Importante",
+    },
+    {
+      titulo: "Programação em C++ Algoritmos, Estrutura de Dados e Objetos",
+      image: image11,
+      autor: "Luiz Joyanes Aguilar",
+      categoria: "Importante",
+    },
+    {
       titulo: "Fundamentos de bancos de dados: Modelagem, projeto e linguagem SQL",
       image: image5,
       autor: "Célio Cardoso Guimarães",
       categoria: "Recomendado",
     },
     {
-      id: 6,
       titulo: "Projeto, Desenvolvimento de Aplicações e Administração de Banco de Dados",
       image: image6,
       autor: "Michael V. Mannino",
--- src/context/assets/index.js
+++ src/context/assets/index.js
@@ -4,5 +4,11 @@ import image3 from "./image3.png";
 import image4 from "./image4.png";
 import image5 from "./image5.png";
 import image6 from "./image6.png";
+import image7 from "./image7.jpg";
+import image8 from "./image8.jpg";
+import image9 from "./image9.jpg";
+import image10 from "./image10.jpg";
+import image11 from "./image11.jpg";
+import image12 from "./image12.jpg";
 
-export { image1, image2, image3, image4, image5, image6 };
+export { image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12 };
