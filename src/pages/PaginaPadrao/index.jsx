277e3274aaf42d500b7149e0cc7130e232437421
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-10-21 12:37:42 -0300

    Hotfix: PaginaPadrao

--- src/pages/PaginaPadrao/index.jsx
+++ src/pages/PaginaPadrao/index.jsx
@@ -1,29 +1,8 @@
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
 import Navegacao from "../../components/Navegacao";
 import { ArtigoProvider, LivroProvider } from "../../context/ProductContext";
-
-const LayoutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  min-width: 1250px;
-  background-color: var(--background-default);
-`;
-
-const Footer = styled.footer`
-  width: 100%;
-  height: 75px;
-  background-color: var(--laranja);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const FooterText = styled.p`
-  color: var(--branco-default);
-`;
+import LayoutContainer from "../../components/LayoutContainer";
+import FooterPersonalizado from "../../components/Footer";
 
 function PaginaPadrao() {
   return (
@@ -32,11 +11,7 @@ function PaginaPadrao() {
         <ArtigoProvider>
           <Navegacao />
           <Outlet />
-          <Footer>
-            <FooterText>
-              © 2023 FreeArtiks. Todos os direitos reservados.
-            </FooterText>
-          </Footer>
+          <FooterPersonalizado />
         </ArtigoProvider>
       </LivroProvider>
     </LayoutContainer>
